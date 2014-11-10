390cdfcb596795d1f807d08435837c383bd8dce5
odoepner <oliver@doepner.net>
2014-11-10 19:32:20 -0400

    Make less assumptions about surrounding div

--- src/main/webapp/profile.js
+++ src/main/webapp/profile.js
@@ -1,25 +1,29 @@
 $(document).ready(function () {
 
-    $(".skill").each(function () {
-        var label = $(this).children("label");
-        var select = $(this).children("select");
+    function txt(element) {
+        return $.trim(element.text());
+    }
+
+    $(".skill > select").each(function () {
+        var select = $(this);
+        var label = $("label[for='" + select.attr('id') + "']");
 
         function selectedOption() {
             return select.children('option:selected');
-        }                
+        }
         select.hide();
 
         label.addClass("button");
-        label.prop('title', $.trim(selectedOption().text()));
+        label.prop('title', txt(selectedOption()));
 
         label.click(function () {
             var next = selectedOption().next();
-            if (next.length == 0) {
+            if (next.length === 0) {
                 next = select.children('option:first');
             }
             next.prop('selected', 'true');
-            label.prop('title', $.trim(next.text()));
+            label.prop('title', txt(next));
         });
-    })
+    });
 
 });
